1a45538edc407ffd9240154d2d0e7b361fadf9be
Yan Nusinovich <yan.nusinovich@gmail.com>
2023-06-12 02:11:24 -0400

    hw3 updates

--- hw3/orchestrate.py
+++ hw3/orchestrate.py
@@ -110,7 +110,7 @@ def train_best_model(
 
 
 @flow
-def main_flow(
+def main_flow_hw(
     train_path: str = "./data/green_tripdata_2021-01.parquet",
     val_path: str = "./data/green_tripdata_2021-02.parquet",
 ) -> None:
@@ -132,4 +132,4 @@ def main_flow(
 
 
 if __name__ == "__main__":
-    main_flow()
+    main_flow_hw()

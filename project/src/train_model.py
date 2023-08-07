f8b7042434e2b6a6437cda8234da19e86dd7b29c
Yan Nusinovich <yan.nusinovich@gmail.com>
2023-08-07 01:40:48 -0400

    training code updates

--- project/src/train_model.py
+++ project/src/train_model.py
@@ -1,12 +1,13 @@
+import logging
+import os
+import pickle
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import mean_squared_error
 import mlflow
 import xgboost as xgb
 from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
 from hyperopt.pyll import scope
-import os
 import pandas as pd
-import logging
 
 logging.basicConfig(level=logging.INFO)
 
@@ -14,52 +15,52 @@ logging.basicConfig(level=logging.INFO)
 class ModelTrainer():
     """"""
 
-    def __init__(self, TRACKING_SERVER_HOST):
+    def __init__(self, tracking_server_host):
         """"""
-        self.TRACKING_SERVER_HOST = TRACKING_SERVER_HOST
+        self.tracking_server_host = tracking_server_host
 
     def load_data(self):
         """Load the data and create X and y"""
         df = pd.read_parquet("../data/df.parquet")
-        self.train_df = pd.read_parquet("../data/train_df.parquet")
-        self.val_df = pd.read_parquet("../data/val_df.parquet")
-        self.test_df = pd.read_parquet("../data/test_df.parquet")
-        self.y = "Methyl tert-butyl ether (MTBE)"
-        self.X_col = [c for c in df.columns if c not in [y, "sample_date", "station_id"]]
-
-    def objective(self, params):
-        with mlflow.start_run():
-            mlflow.set_tag("model", "xgboost")
-            mlflow.log_params(params)
-            booster = xgb.train(
-                params=params,
-                dtrain=train,
-                num_boost_round=1000,
-                evals=[(valid, 'validation')],
-                early_stopping_rounds=50
-                )
-            y_pred = booster.predict(valid)
-            rmse = mean_squared_error(y_val, y_pred, squared=False)
-            mlflow.log_metric("rmse", rmse)
-            with open("models/preprocessor.b", "wb") as f_out:
-                pickle.dump(dv, f_out)
-            mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")     
-        return {'loss': rmse, 'status': STATUS_OK}
+        train_df = pd.read_parquet("../data/train_df.parquet")
+        val_df = pd.read_parquet("../data/val_df.parquet")
+        y = "Methyl tert-butyl ether (MTBE)"
+        X_col = [c for c in df.columns if c not in [y, "sample_date", "station_id"]]
+        return train_df, val_df, y, X_col
 
     def run_training(self):
         """"""
+        train_df, val_df, y, X_col = self.load_data()
         dv = DictVectorizer()
-        train_dicts = self.train_df[self.X_col].to_dict(orient='records')
+        train_dicts = train_df[X_col].to_dict(orient='records')
         X_train = dv.fit_transform(train_dicts)
-        val_dicts = self.val_df[self.X_col].to_dict(orient='records')
+        val_dicts = val_df[X_col].to_dict(orient='records')
         X_val = dv.transform(val_dicts)
-        y_train = self.train_df[self.y].values
-        y_val = self.val_df[self.y].values
+        y_train = train_df[y].values
+        y_val = val_df[y].values
         train = xgb.DMatrix(X_train, label=y_train)
         valid = xgb.DMatrix(X_val, label=y_val)
-        mlflow.set_tracking_uri(f"http://{self.TRACKING_SERVER_HOST}:5000")
+        mlflow.set_tracking_uri(f"http://{self.tracking_server_host}:5000")
         mlflow.set_experiment("water-quality-prediction")
         mlflow.xgboost.autolog()
+        def objective(params):
+            with mlflow.start_run():
+                mlflow.set_tag("model", "xgboost")
+                mlflow.log_params(params)
+                booster = xgb.train(
+                    params=params,
+                    dtrain=train,
+                    num_boost_round=1000,
+                    evals=[(valid, 'validation')],
+                    early_stopping_rounds=50
+                    )
+                y_pred = booster.predict(valid)
+                rmse = mean_squared_error(y_val, y_pred, squared=False)
+                mlflow.log_metric("rmse", rmse)
+                with open("models/preprocessor.b", "wb") as f_out:
+                    pickle.dump(dv, f_out)
+                mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
+            return {'loss': rmse, 'status': STATUS_OK}
         search_space = {
             'max_depth': scope.int(hp.quniform('max_depth', 4, 100, 1)),
             'learning_rate': hp.loguniform('learning_rate', -3, 0),
@@ -70,24 +71,25 @@ class ModelTrainer():
             'seed': 42
             }
         best_result = fmin(
-            fn=self.objective,
+            fn=objective,
             space=search_space,
             algo=tpe.suggest,
             max_evals=50,
             trials=Trials()
             )
+        return best_result
 
 
-def train_model(TRACKING_SERVER_HOST):
+def train_model(tracking_server_host):
     """Main function for model training."""
-    model_trainer = ModelTrainer(TRACKING_SERVER_HOST)
+    model_trainer = ModelTrainer(tracking_server_host)
     model_trainer.load_data()
     model_trainer.run_training()
 
 
 if __name__ == "__main__":
     os.environ["AWS_PROFILE"] = "default"
-    TRACKING_SERVER_HOST= "ec2-54-147-5-224.compute-1.amazonaws.com"
+    TRACKING_SERVER_HOST = "ec2-54-147-5-224.compute-1.amazonaws.com"
 
     directory = os.path.dirname(os.path.abspath(__file__))
     os.chdir(directory)
